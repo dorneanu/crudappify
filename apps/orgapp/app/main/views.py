769433d5eee4540a09041489a849b7f595f3f4b4
Victor Dorneanu <info@dornea.nu>
2015-01-25 15:05:07 +0100

    Added selected tags (primary, secondary) feature

--- apps/orgapp/app/main/views.py
+++ apps/orgapp/app/main/views.py
@@ -23,6 +23,7 @@ def analysis(table=None):
 
     elif table == "targets":
         return render_template('analysis/target.html')
+
     else:
         return "<p>ERROR</p>"
 
@@ -56,6 +57,30 @@ def targets(mode=None):
 
         return jsonify(json_dict)
 
+    # Return priorities
+    elif mode == "priority":
+        from sqlalchemy import func
+        colors = {'high': '#FD0000', 'medium': '#FF8000', 'low': '#ffff00', 'none': '#eeeeee'}
+        json_prio = []
+        # Return priority count for all targets
+        priorities = db_session.query(Target.priority, func.count(Target.priority)).group_by(Target.priority).all()
+        for p in priorities:
+            if p[0]:
+                json_prio.append({'label': p[0], 'value': p[1], 'color': colors[p[0].lower()]})
+            else:
+                json_prio.append({'label': 'None', 'value': p[1], 'color': colors['none']})
+
+
+        # Build dict
+        json_dict = {
+            "priorities": json_prio,
+            "discrete": [{ "key": "Cumulative Return", "values": json_prio }]
+        }
+
+        return jsonify(json_dict)
+
+
+
     # Return records
     elif mode == "table":
         serialized = srlz.TargetSerializer(targets.all(), many=True);
@@ -80,12 +105,46 @@ def applications(mode=None):
 
         return jsonify(json_dict)
 
+    # Return severities
+    elif mode == "severity":
+        from sqlalchemy import func
+        json_severity = []
+        colors = {'high': '#FD0000', 'medium': '#FF8000', 'low': '#ffff00', 'none': '#eeeeee'}
+
+        # Return severity count for all apps
+        severities = db_session.query(App.severity, func.count(App.severity)).group_by(App.severity).all()
+        for s in severities:
+            if s[0]:
+                json_severity.append({'label': s[0], 'value': s[1], 'color': colors[s[0].lower()]})
+            else:
+                json_severity.append({'label': 'None', 'value': s[1], 'color': colors['none']})
+
+
+        # Build dict
+        json_dict = {
+            "severities": json_severity,
+            "discrete": [{ "key": "Cumulative Return", "values": json_severity }]
+        }
+
+        return jsonify(json_dict)
+
     # No mode specified
     else:
         return 'No mode specified'
 
+@apps.route('/api/get/selected-tags', methods = ['POST'])
+def selected_tags():
+    tags = request.json
+    groups = []
 
+    for t in tags:
+        # Look for first group
+        apps = db_session.query(App).filter(App.tags.any(Tag.name.in_(t['main'])))
 
+        # Look for sub-groups
+        for m in t['main']:
+            for g in t['sub']:
+                c = apps.filter(App.tags.any(Tag.name.in_([g]))).count()
+                groups.append({'group': m, 'label':g, 'value':c})
 
-
-
+    return json.dumps(groups)
